c0b596cbddde24975a806fcd83f6346248acadf4
Lucas Alderfer <lucas.alderfer@gmail.com>
2018-08-15 20:42:04 -0600

    Adds all revenue for a merchant on one day

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -1,7 +1,13 @@
 class Api::V1::Merchants::RevenueController < ApplicationController
 
   def index
-    render json: Merchant.select('merchants.*, sum(invoice_items.quantity * invoice_items.unit_price) AS revenue').joins(:invoices => [:invoice_items, :transactions]).where(transactions: {result: "success"}).group(:merchant_id, :id).find(params[:id]), serializer: MerchantRevenueSerializer
+    unless params[:date].nil?
+      date = Date.parse(params[:date])
+      range = date.midnight..(date.midnight + 1.day)
+      render json: Merchant.select("merchants.*, sum(invoice_items.quantity * invoice_items.unit_price) AS revenue").joins(:invoices => [:invoice_items, :transactions]).where(transactions: {result: "success"}).where(invoices: {created_at: range}).group(:merchant_id, :id).find(params[:id]), serializer: MerchantRevenueSerializer
+    else
+      render json: Merchant.select('merchants.*, sum(invoice_items.quantity * invoice_items.unit_price) AS revenue').joins(:invoices => [:invoice_items, :transactions]).where(transactions: {result: "success"}).group(:merchant_id, :id).find(params[:id]), serializer: MerchantRevenueSerializer
+    end
   end
 
 end
